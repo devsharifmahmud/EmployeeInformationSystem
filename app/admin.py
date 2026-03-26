e8c0a29de7feb2b4e42cb622e7cdc67de9e9ea60
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-03-26 11:40:51 +0600

    Update Code

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Employee
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = '__all__'
 
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
