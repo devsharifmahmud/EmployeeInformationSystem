eb84adb80197ae1748040a658f7a1daf5ab4200a
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-04-04 19:31:50 +0600

    Update Code

--- app/views.py
+++ app/views.py
@@ -4,4 +4,7 @@ from forms import EmployeeForm
 # Create your views here.
 def Home(request):
     form = EmployeeForm()
+    context = {
+        'form': form,
+    }
     return render(request, 'apps/index.html')
