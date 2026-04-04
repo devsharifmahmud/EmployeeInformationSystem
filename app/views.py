f9b056d56fe42ce364561193e9313af202b97f9d
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-04-04 19:31:11 +0600

    Update Code

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from forms import EmployeeForm
 
 # Create your views here.
 def Home(request):
+    form = EmployeeForm()
     return render(request, 'apps/index.html')
